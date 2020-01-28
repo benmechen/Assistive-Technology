cfdfae749ce71f82686f5498212ecff308faf419
Ben Mechen <psybm7@nottingham.ac.uk>
2020-01-28 14:35:13 +0000

    Create Xcode project

---
+++ Assistive Technology/Assistive Technology/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assistive Technology
+//
+//  Created by Ben Mechen on 28/01/2020.
+//  Copyright © 2020 Team 30. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
