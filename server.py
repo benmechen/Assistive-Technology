4682693355bb7941a60346d475ba7f9a1e253afa
Ben Mechen <psybm7@nottingham.ac.uk>
2020-02-04 18:36:52 +0000

    Tidy server.py, shutdown Bonjour service on exti

--- server.py
+++ server.py
@@ -1,18 +1,3 @@
-# import random
-# import socket
-#
-# server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# server_socket.bind(('', 12000))
-#
-# while True:
-#     rand = random.randint(0, 10)
-#     message, address = server_socket.recvfrom(1024)
-#     message = message.upper()
-#     print(message.decode('utf-8'))
-#     if rand >= 4:
-#         server_socket.sendto(message, address)
-
-
 import socket
 import netifaces as ni
 import datetime
@@ -26,6 +11,7 @@ def get_ip_address(ifname):
     ni.ifaddresses(ifname)
     # return "127.0.0.1"
     return ni.ifaddresses(ifname)[ni.AF_INET][0]['addr']
+
 
 print("#####################################")
 print("#### ASSISTIVE TECHNOLOGY SERVER ####")
@@ -46,23 +32,26 @@ hostname = fqdn.split('.')[0]
 desc = {'service': 'Assistive Technology Service', 'version': '1.0.0'}
 info = ServiceInfo('_assistive-tech._udp.local.',
                     hostname + ' Assistive Technology Server._assistive-tech._udp.local.',
-                    socket.inet_aton(ip_addr), 1024, 0, 0,
+                    socket.inet_aton(ip_addr), 1025, 0, 0,
                     desc, hostname + '.local.')
-zeroconf.register_service(info)
-print(" > Zeroconf service " + str(desc) + " registered:\n" + str(info))
+try:
+    zeroconf.register_service(info)
+    print(" > Zeroconf service " + str(desc) + " registered:\n" + str(info))
 
-while True:
-    message, address = serverSocket.recvfrom(1024)
-    string = message.decode('utf-8')
-    print(" > Received: " + message.decode('utf-8') + " from " + str(address))
-    serverSocket.sendto("astv_ack".encode('utf-8'), address)
-    if string == "astv_discover":
-        returnMessage = "astv_shake:" + get_ip_address('en0')
-        print(" > Discover call from client: " + str(address))
-        print(" > Sending handshake: " + returnMessage + ", to address: " + str(address))
-        serverSocket.sendto(returnMessage.encode('utf-8'), address)
-        print(" > [" + str(datetime.datetime.now()) + "] New client connected <" + address[0] + ">")
-    elif string == "astv_disconnect":
-        zeroconf.close()
-        serverSocket.close()
-        break
+    while True:
+        message, address = serverSocket.recvfrom(1024)
+        string = message.decode('utf-8')
+        print(" > Received: " + message.decode('utf-8') + " from " + str(address))
+        serverSocket.sendto("astv_ack".encode('utf-8'), address)
+        if string == "astv_discover":
+            returnMessage = "astv_shake:" + get_ip_address('en0')
+            print(" > Discover call from client: " + str(address))
+            print(" > Sending handshake: " + returnMessage + ", to address: " + str(address))
+            serverSocket.sendto(returnMessage.encode('utf-8'), address)
+            print(" > [" + str(datetime.datetime.now()) + "] New client connected <" + address[0] + ">")
+        elif string == "astv_disconnect":
+            break
+finally:
+    zeroconf.unregister_service(info)
+    zeroconf.close()
+    serverSocket.close()
