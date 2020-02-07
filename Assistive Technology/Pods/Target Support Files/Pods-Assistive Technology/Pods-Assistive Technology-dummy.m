dd9549b6e412d8164a2a1a287290d78aa2c09494
Ben Mechen <psybm7@nottingham.ac.uk>
2020-02-07 17:34:16 +0000

    Tidy up folders

--- Assistive Technology/Pods/Target Support Files/Pods-Assistive Technology/Pods-Assistive Technology-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Assistive_Technology : NSObject
-@end
-@implementation PodsDummy_Pods_Assistive_Technology
-@end
--- Assistive Technology/Pods/Target Support Files/Pods-Assistive TechnologyTests/Pods-Assistive TechnologyTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Assistive_TechnologyTests : NSObject
-@end
-@implementation PodsDummy_Pods_Assistive_TechnologyTests
-@end
