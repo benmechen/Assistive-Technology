eb67a884691f432f989e0f51be7e79e627ff254f
Walter A. Cambindo M <psywac@nottingham.ac.uk>
2020-02-26 21:36:59 +0000

    Created c# visual studio project - Client

---
+++ Windows/Client/Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zeroconf;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
