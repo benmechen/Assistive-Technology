32afb90b7a4dfc2ebc3f8c9925d799a1f10df242
Walter A. Cambindo M <psywac@nottingham.ac.uk>
2020-02-26 21:39:52 +0000

    created basic client with zeroconf

--- Windows/Client/Client/Program.cs
+++ Windows/Client/Client/Program.cs
@@ -9,12 +9,40 @@ using System.Net.Sockets;
 
 namespace Client
 {
+    class NetworkServices
+    {
+        public async Task ProbeForServices()
+        {
+            IReadOnlyList<IZeroconfHost> results = await
+                ZeroconfResolver.ResolveAsync("_assistive-tech._udp.local.");
+            foreach (var resp in results)
+                Console.WriteLine(resp);
+
+        }
+
+        public async Task EnumerateAllServicesFromAllHosts()
+        {
+            ILookup<string, string> domains = await ZeroconfResolver.BrowseDomainsAsync();
+            var responses = await ZeroconfResolver.ResolveAsync(domains.Select(g => g.Key));
+
+            foreach (var resp in responses)
+                Console.WriteLine(resp);
+
+            Console.WriteLine("Done!");
+        }
+    }
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
 
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Client!");
+            NetworkServices p = new NetworkServices();
+
+            Console.WriteLine("Probing for assistive tech services: \n");
+            await p.ProbeForServices();
+            Console.WriteLine("General Probing: ");
+            await p.EnumerateAllServicesFromAllHosts();
             Console.ReadLine();
         }
     }
