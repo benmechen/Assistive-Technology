3401d29335d1adf0479a3b2a26c814d36e7555c1
Walter A. Cambindo M <psywac@nottingham.ac.uk>
2020-02-27 19:49:47 +0000

    created new service publisher using arkane.zeroconf package

--- Windows/Server/Server/Program.cs
+++ Windows/Server/Server/Program.cs
@@ -11,7 +11,24 @@ namespace Zeroconf
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Sample service publisher using arkane.Mono.Zeroconf version\n");
+
+            RegisterService service = new RegisterService();
+            service.Name = "Assistive Technology Server";
+            service.RegType = "_assistive-tech._udp";
+            service.ReplyDomain = "local.";
+            service.Port = 1024;
+
+
+            TxtRecord txt_record = new TxtRecord();
+            txt_record.Add("service", "Assistive Technology Technology");
+            txt_record.Add("version", "1.0.0");
+            service.TxtRecord = txt_record;
+            service.Register();
+            Console.WriteLine("Server properties have been registered");
+            Console.ReadLine();
+            service.Dispose();
+            Console.WriteLine("service has been disposed! Bye!!!!\n");
         }
     }
 }
