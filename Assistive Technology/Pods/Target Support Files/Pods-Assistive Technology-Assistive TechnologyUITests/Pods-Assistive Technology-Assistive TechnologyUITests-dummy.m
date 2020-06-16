ba0ea66094b5caa7746f7ca471d4b052ba7c0d39
Ben Mechen <psybm7@nottingham.ac.uk>
2020-06-16 08:54:18 +0100

    Clean up master for merge

--- Assistive Technology/Pods/Target Support Files/Pods-Assistive Technology-Assistive TechnologyUITests/Pods-Assistive Technology-Assistive TechnologyUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Assistive_Technology_Assistive_TechnologyUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Assistive_Technology_Assistive_TechnologyUITests
-@end
